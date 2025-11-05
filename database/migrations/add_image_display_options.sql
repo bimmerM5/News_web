29fa707de72f587258263d6c31c3d233efa2e9ea
dahikuv <phamtrunghieukg2005@gmail.com>
2025-11-05 22:08:56 +0700

    Update frontend and backend

--- database/migrations/add_image_display_options.sql
+++
@@ -1,5 +0,0 @@
--- Add display options for article images
-ALTER TABLE `article_media`
-    ADD COLUMN `size_class` VARCHAR(20) NOT NULL DEFAULT 'img-medium',
-    ADD COLUMN `align_class` VARCHAR(20) NOT NULL DEFAULT 'img-center',
-    ADD COLUMN `caption` TEXT NULL;
